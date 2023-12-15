738f192c3ccedc57c17a8bdcb922d30eff1b55c1
erdUha <theerden.kenzhebaev.2006@gmail.com>
2023-12-15 09:19:33 +0600

    backind

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,46 @@
 const express = require('express'); // Подключение Express.js
-const session = require('express-session');
+const session = require('express-session'); // Сессия express
 const router = express.Router(); // Подключение роутера
 
-const pg = require('pg'); // Подключение модуля pg, для подключения базы данных от PostgreSQL
-const db = require('./db'); //
+const db = require('./db'); // Подключение базы данных
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // Необходим для хеширования и сравнения паролей
+const Verifier = require("email-verifier"); // Для глубинной проверки эл. почты
 
 const app = express(); // Создание бекенд приложения
+app.set('view engine', 'pug'); // Присвоение шаблонизатора "pug" приложению. Стандартная папка для шаблонов "./views"
+
+// Конфигурации сессии
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/dist")) // Указание папки "dist" для static файлов
+app.use(express.static(__dirname + "/static")) // Указание папки "static" для static файлов
+
 
 module.exports = router;
 
 // Функция хеширования паролей
-const hashPassword = async (password, saltRounds = 12) => {
+const hashPassword = async (password, saltRounds = 14) => { // saltRounds должен быть не меньше 10 для защиты от брут-форса!
 	try {
-		// Генерация соли
-		const salt = await bcrypt.genSalt(saltRounds);
-		// Создание хеша
-		return await bcrypt.hash(password, salt);
+		const salt = await bcrypt.genSalt(saltRounds) // Генерация соли
+		return await bcrypt.hash(password, salt); // Создание хеша (String)
 	} catch (err) { // Поимка ошибок
 		console.error(err);
 	}
 	return null;
 }
 
-// Функция проверки пароля
+// Функция сверки пароля и хеша
 const comparePassword = async (password, hash) => {
   try {
-    // Проверка
-    return await bcrypt.compare(password, hash)
+    return await bcrypt.compare(password, hash) // Проверка (true/false)
   } catch (error) { // Поимка ошибок
     console.log(error)
   }
@@ -38,66 +50,62 @@ const comparePassword = async (password, hash) => {
 // Функция проверки эл. почты
 function isValidEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
+  return regex.test(email); // (true/false)
 }
 
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(__dirname + "/dist")) // Указание папки "dist" для static файлов
-
-app.get('/login', async (req, res) => { // Подключение веб приложения из папки "dist"
-  res.sendFile(__dirname + "/login/index.html");
+app.get('/login', async (req, res) => { // Вывод на экран страницы входа
+	res.render('login');
 });
 
-app.get('/signup', async (req, res) => { // Подключение веб приложения из папки "dist"
-  res.sendFile(__dirname + "/regestration/regestration.html");
+app.get('/signup', async (req, res) => { // Вывод на экран страницы регистрации
+	res.render('signup');
 });
 
 
-app.post('/auth', async (req, res) => { // Авторизация
+app.post('/login', async (req, res) => { // Авторизация
 	// Сбор информации из запроса
 	let username = req.body.username;
 	let password = req.body.password;
 	let isEmail = isValidEmail(username);
+
 	// Проверка на случай пустых полей
 	if (username && password) {
 		try {
-			// Произведение PostgeSQL запроса
 			let hashedPassword;
 			let query = "";
-			if (isEmail) {
-				query = "SELECT password FROM users WHERE email = '" + username  + "'";
+			if (isEmail) { // Выборка запроса в зависимости, если пользователь ввел эл. почту или имя пользователя
+				query = "SELECT * FROM users WHERE email = '" + username  + "'";
 			} else {
-				query = "SELECT password FROM users WHERE username = '" + username  + "'";
+				query = "SELECT * FROM users WHERE username = '" + username  + "'";
 			}
-			const response = await db.query(query);
-			hashedPassword = response.rows[0].password;
-			if (hashedPassword.length != 0) { // Проверка на наличие пользователя 
-				;(async () => {
-					const isValidPass = await comparePassword(password, hashedPassword);
-					if (isValidPass) {
-						res.send("Вход произведен успешно!");
-					}	else {
-						res.send("Неправильный пароль");
-					}
-				})()
-			} else { // Иначе
-				res.send("Неправильное эл. почта или имя пользователя!");
+			const response = await db.query(query); // Произведение PostgeSQL запроса для проверки пароля
+			// Проверка на наличие пользователя в базе данных
+			if (response.rows[0]) {
+				hashedPassword = response.rows[0].password; // Получение хешированного пароля для сверки
+			} else {
+				res.render('login', { message: "Неправильное имя пользователя или пароль" });
+				return false;
 			}
+			;(async () => {
+				const isValidPass = await comparePassword(password, hashedPassword); // Сравнение введенного пароля и хешированного
+				// Проверка входа
+				if (isValidPass) {
+					res.redirect('/');
+				}	else {
+					res.render('login', { message: "Неправильное имя пользователя или пароль" });
+				}
+			})()
 		} catch (err) { // Поимка ошибок и вывод в консоль
+			res.render('login', { message: "Ошибка, пропробуйте позже" });
 			console.error(err);
-			console.error("THIS WAS ERROR");
+			return false;
 		}
+	} else {
+		res.render('login', { message: "Пожалуйса, заполните поля" });
 	}
 });
 
-app.post('/regestration', async (req, res) => { // Регистрация
+app.post('/signup', async (req, res) => { // Регистрация
 	// Сбор информации из запроса
 	const username = req.body.username;
 	const password = req.body.password;
@@ -105,7 +113,12 @@ app.post('/regestration', async (req, res) => { // Регистрация
 
 	// Проверка на случай пустых полей
 	if (username && password) {
-		// Проверка эл. почты
+		// Проверка валидности эл. почты
+		if (!isValidEmail(email)) {
+			res.return("signup", { message: "Пожалуйста, введите правильную Эл. почту" });
+			return 0;
+		} else {
+		}
 		;(async () => {
 			const hashedPassword = await hashPassword(password);
 			let keys = "";
@@ -121,19 +134,20 @@ app.post('/regestration', async (req, res) => { // Регистрация
 				const isEmailAvailable = await db.query("SELECT * FROM users WHERE email = '" + email + "'");
 				const isUsernameAvailable = await db.query("SELECT * FROM users WHERE username = '" + username + "'");
 				if (isEmailAvailable.rows.length != 0) {
-					res.send("Эл. почта уже занята, попробуйте войти в аккаунт");
+					res.send("<h1 style='color: red'>Эл. почта уже занята, попробуйте войти в аккаунт</h1>");
 					return false;
 				}
 				if (isUsernameAvailable.rows.length != 0) {
-					res.send("Имя пользователя уже занято");
+					res.send("<h1 style='color: red'>Имя пользователя уже занято</h1>");
 					return false;
 				}
 				// Произведение PostgeSQL запроса для внедрения информации о пользователе
 				await db.query("INSERT INTO users(" + keys + ") VALUES(" + values + ")");
 				res.redirect('/');
 			} catch (err) { // Поимка ошибок и вывод в консоль
+				res.render('signup', { message: "Ошибка, пропробуйте позже" });
 				console.error(err);
-				console.error("THIS WAS ERROR");
+				return false;
 			}
 		})()
 	}
@@ -143,16 +157,6 @@ app.post('/regestration', async (req, res) => { // Регистрация
 app.get('*', async (req, res) => { 
   res.sendFile(__dirname + "/dist/index.html");
 });
-
-//app.get('/', async (req, res) => {
-//  try {
-//    const result = await db.query('SELECT * FROM person');
-//    res.json(result.rows);
-//  } catch (err) {
-//    console.error(err);
-//    res.status(500).send('Internal Server Error');
-//  }
-//});
 
 const port = 3333;
 
